0e3d2a95aee5b32c160e8d0e21f1cc72422729f4
Sasha F <sasha.friedenberg@datadoghq.com>
2024-08-01 12:01:55 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #! /bin/bash -xe
 
 # update fish
-sudo apt-add-repository ppa:fish-shell/release-3
-sudo apt update
-sudo apt install fish
+sudo apt-add-repository -y ppa:fish-shell/release-3
+sudo apt update -y
+sudo apt install -y fish
 
 # install nix
 sh <(curl -L https://nixos.org/nix/install) --no-daemon --yes
@@ -13,7 +13,7 @@ sh <(curl -L https://nixos.org/nix/install) --no-daemon --yes
 
 # clone actual dotfiles
 git clone https://github.com/friedenberg/dotfiles ~/.dotfiles
-git clone git@github.com:mummifiedsierra346/dotfiles-private.git ~/.dotfiles/tag-datadog_private
+# git clone git@github.com:mummifiedsierra346/dotfiles-private.git ~/.dotfiles/tag-datadog_private
 
 # build nix dotfiles stuff
 pushd ~/.dotfiles
@@ -23,8 +23,7 @@ bin_fish="$(readlink "$bin_result/fish")"
 export PATH="$bin_result:$PATH"
 
 cp rcrc ~/.rcrc
-printf "DOTFILES_DIRS=\"%s\"" "$(pwd)" >> ~/.rcrc
-"$bin_result/rcup" -f
+rcup -f
 
 # sudo bash -c "echo '$bin_fish' >> /etc/shells"
 # sudo chsh -s "$bin_fish"
